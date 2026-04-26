1d88e6dbbaa652aa2081dab9f3943d226fa981b3
Lucas Lautaro Mamani <lucasxz5lm@gmail.com>
2026-04-26 07:36:08 -0300

    tp 2 ejercicios 4,10,17.

---
+++ js/ejercicio4.js
@@ -0,0 +1,8 @@
+const lado1 = 10;
+const lado2 = 5;
+
+const area = lado1 * lado2;
+const perimetro = 2 * (lado1 + lado2);
+
+console.log(`Área: ${area} cm`);
+console.log(`Perímetro: ${perimetro} cm`);
