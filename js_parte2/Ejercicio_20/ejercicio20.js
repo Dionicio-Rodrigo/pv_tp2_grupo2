a845f221c6bfd7d21c0ad030feaaabf35b15f3f5
Dio <47392641@fi.unju.edu.ar>
2026-04-29 17:50:45 -0300

    Formalizo los archivos html y js

--- js_parte2/Ejercicio_20/ejercicio20.js
+++ js_parte2/Ejercicio_20/ejercicio20.js
@@ -4,8 +4,10 @@ const lu = document.querySelector(".contenedorInput > #Lu");
 const btnIngresar = document.querySelector("button");
 
 btnIngresar.addEventListener("click", () => {
-  alert(`Los datos ingresados son:
+  if (nombre.value != "" && apellido.value != "" && lu.value != "") {
+    alert(`Los datos ingresados son:
     Nombre: ${nombre.value}  
     Apellido: ${apellido.value}  
     Libreta Universitaria: ${lu.value}`);
+  }
 });
